e911d9ea2d9136bb254ba84371d86313c561721f
aesopcmc <chao_c_c@163.com>
2024-06-17 23:26:14 +0800

    修改quartz 调度程序一次性触发的触发器数量为50

--- Core/src/main/java/top/mcos/scheduler/jobs/RegenWorldJob.java
+++ Core/src/main/java/top/mcos/scheduler/jobs/RegenWorldJob.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Deprecated
 //@DisallowConcurrentExecution
 public class RegenWorldJob extends AbstractJob<RegenWorldConfig> {
     @Override
